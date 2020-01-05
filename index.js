9df22c633e723c2ecbb6360cb158c36137260eb7
Ash Kyd <ash@kyd.com.au>
2020-01-06 00:39:53 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const rimraf = require("rimraf");
+const { ffmpeg, info, encodeRenditions } = require("./lib/ffmpeg");
+const input =
+  "/Users/ash/Downloads/REVIEW-%20Amsterdam%20Light%20Festival%20-%2019%20artworks.mp4";
+const output = "/Users/ash/Web/video/output/";
+const renditions = require("./renditions.json");
+
+rimraf.sync(output);
+fs.mkdirSync(output);
+
+const action = encodeRenditions({ input, renditions: renditions, output });
+action.on("progress", progress =>
+  console.log(
+    String(progress.percent) + "%",
+    Math.round(progress.eta / 60),
+    "minutes remaining"
+  )
+);
